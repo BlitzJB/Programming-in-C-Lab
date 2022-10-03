6acfc8dde73db5688383ef427d16059420f5bcc8
Joshua Bharathi <blitz04.dev@gmail.com>
2022-10-03 03:58:03 +0000

    total and avg program

---
+++ EXP2/totalavg.c
@@ -0,0 +1,32 @@
+/* #include <stdio.h>
+
+int mark1;
+int mark2;
+int mark3;
+int mark4;
+int mark5;
+
+int total = 0;
+float avg = 0;
+
+int main(void) {
+
+  printf("Enter your marks1: ");
+  scanf("%d", &mark1);
+  printf("Enter your marks2: ");
+  scanf("%d", &mark2);
+  printf("Enter your marks3: ");
+  scanf("%d", &mark3);
+  printf("Enter your marks4: ");
+  scanf("%d", &mark4);
+  printf("Enter your marks5: ");
+  scanf("%d", &mark5);
+
+  total = mark1 + mark2 + mark3 + mark4 + mark5;
+  avg = total / 5;
+
+  printf("Total Marks: %d\n", total);
+  printf("Avg Marks: %.2f\n", avg);
+
+  return 0;
+} */
