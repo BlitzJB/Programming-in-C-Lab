cf134a8df588a6bac4604db1b357421ce2230988
Joshua Bharathi <blitz04.dev@gmail.com>
2022-10-03 03:50:06 +0000

    swap program

---
+++ EXP2/swap.c
@@ -0,0 +1,23 @@
+/* #include <stdio.h>
+
+int number1 = 0;
+int number2 = 0;
+int temp = 0;
+
+int main(void) {
+
+  printf("Enter your number 1: ");
+  scanf("%d", &number1);
+
+  printf("Enter your number 2: ");
+  scanf("%d", &number2);
+
+  temp = number2;
+  number2 = number1;
+  number1 = temp;
+
+  printf("By Using Temp Variable\n");
+  printf("Num1: %d, Num2: %d\n", number1, number2);
+
+  return 0;
+} */
