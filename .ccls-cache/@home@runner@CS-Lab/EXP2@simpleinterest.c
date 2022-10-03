62b2b2cf93ad277164e9185d56d1817bf39a20fe
Joshua Bharathi <blitz04.dev@gmail.com>
2022-10-03 04:15:20 +0000

    simple interest

---
+++ .ccls-cache/@home@runner@CS-Lab/EXP2@simpleinterest.c
@@ -0,0 +1,18 @@
+/* #include <stdio.h>
+
+float P, N, R;
+float interest_component, final_amount;
+
+int main(void) {
+  printf("Enter Principal: ");
+  scanf("%f", &P);
+  printf("Enter Term in Years: ");
+  scanf("%f", &N);
+  printf("Enter Rate of Interest: ");
+  scanf("%f", &R);
+  interest_component = P * N * (R / 100);
+  final_amount = P + interest_component;
+  printf("Total Interest: %.2f\n", interest_component);
+  printf("Final amount: %.2f", final_amount);
+  return 0;
+} */
